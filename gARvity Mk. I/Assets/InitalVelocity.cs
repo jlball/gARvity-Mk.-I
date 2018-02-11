be35bfa3c87d49b09e6cae38e12d101dcfaa91e3
jlball <32344022+jlball@users.noreply.github.com>
2018-02-11 14:14:45 -0500

    Initial Commit
    All the files in the first iteration

---
+++ gARvity Mk. I/Assets/InitalVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitalVelocity : MonoBehaviour {
+
+    public Transform trAttractor;
+    public float forceIntensity = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        transform.LookAt(trAttractor);
+        Vector3 forceTangent = new Vector3(Random.value, Random.value, 0);
+        Debug.Log(forceTangent);
+
+        rb.AddRelativeForce(forceTangent * forceIntensity, ForceMode.VelocityChange);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ gARvity Mk. I/Assets/Scripts/Gravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    public Rigidbody rbAttractor;
+    private Rigidbody rbLocal;
+    private float rSquared;
+    public float forceScale = 10000.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        rbLocal = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Generates a Vector3 directionOfForce to store the vector that points between the two objects being attracted
+        Vector3 directionOfForce = rbLocal.position - rbAttractor.position;
+
+        // Calcualtes the square of the current distance between the two objects
+        rSquared = Vector3.Distance(rbAttractor.position, rbLocal.position);
+        rSquared = rSquared * rSquared;
+        //Debug.Log(rSquared);
+
+        // Calcuates and applies a force in the direction of the vector between the two objects divided by the square of the distance
+        float forceScaleCalculated = forceScale / rSquared;
+        rbLocal.AddForce(-directionOfForce * forceScaleCalculated);
+
+	}
+}
